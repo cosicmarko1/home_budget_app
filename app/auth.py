4e34d12fa7e581de832fc21ac2c7222e1e1a89f1
Marko Ćosić <cosicmarko1@Mac.home>
2025-06-13 22:49:59 +0200

    Added authentication tests, added pre-commit hook

--- app/auth.py
+++ app/auth.py
@@ -16,11 +16,14 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
+
 def hash_password(password: str):
     return pwd_context.hash(password)
 
+
 def verify_password(plain, hashed):
     return pwd_context.verify(plain, hashed)
+
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
@@ -28,20 +31,21 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
+
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials"
+        detail="Could not validate credentials",
     )
 
-    try: 
+    try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
-    
+
     db = SessionLocal()
     user = db.query(User).filter(User.username == username).first()
     db.close()
